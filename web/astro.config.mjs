c68b58f3c3d73075301a5d4e5e8fca77884db3b5
predikdev <social.predik@gmail.com>
2025-07-23 19:35:01 +0200

    build: Allow external access for local device testing

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -4,8 +4,12 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  server: { host: true },
   vite: {
     envDir: '../',
     plugins: [tailwindcss()],
+    server: {
+      allowedHosts: ['6bb3af06cfe8.ngrok-free.app'],
+    },
   },
 });
